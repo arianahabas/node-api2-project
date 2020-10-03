ddd9be1140f6c3923705afc015b526e01c579a91
Ariana Habas <65680650+arianahabas@users.noreply.github.com>
2020-10-03 11:15:07 -0500

    almost MVP

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,17 +1,56 @@
 const express = require("express")
+const { findById } = require("../data/db")
 
 const router = express.Router()
-const db = require('../data/db')
 const posts = require('../data/db')
 
-//POST - Creates a post using the information sent inside the request body
+//POST - Creates a post using the information sent inside the request body - DONE
+router.post('/api/posts' , (req,res) =>{
+    if(!req.body.title || !req.body.contents){
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    } else {
+        posts.insert(req.body)
+        .then((post)=>{
+            res.status(201).json(post)
+        })
+        .catch((error)=>{
+            console.log(error)
+            res.status(500).json({
+              error: "There was an error while saving the post to the database",
+            })
+        })
+    }   
+})
 
+//POST - Creates a comment for the post with the specified id using information sent inside of the request body. --ERROR
+router.post('/api/posts/:id/comments', (req,res) => {
+    const id = req.params.id
+    const post = posts.findById(id)
+    if(!req.body.text){
+        res.status(400).json({
+            errorMessage: "Please provide text for the comment."
+        })
+    } else if (id != post){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist."
+        })
+    } else {
+        posts.insertComment(req.body)
+        .then((comment)=>{
+            res.status(201).json(comment)
+        })
+        .catch((error)=>{
+            console.log(error)
+            res.status(500).json({
+                error: "There was an error while saving the post to the database",
+            })
+        })
+    }
+})
 
-//POST - Creates a comment for the post with the specified id using information sent inside of the request body.
-
-
-//GET - Returns an array of all the post objects contained in the database. 
-
+//GET - Returns an array of all the post objects contained in the database. -DONE
 router.get('/api/posts', (req,res) =>{
     posts.find()
     .then((post)=> {
@@ -25,17 +64,75 @@ router.get('/api/posts', (req,res) =>{
     })
 })
 
-//GET - Returns the post object with the specified id.
+//GET - Returns the post object with the specified id. --STATUS 404 not working
+router.get('/api/posts/:id',  (req, res) => { 
+    posts.findById(req.params.id)
+    .then((post)=>{
+        if (post) {
+            res.status(200).json(post)
+       
+        } else {
+            //status 400 not working - returns  an empty array
+            res.status(404).json({
+                message:'Post not found'
+            })
+        }
+    })
+    .catch((err)=>{
+        console.log(error)
+			res.status(500).json({
+				message: "Error retrieving the post",
+			})
+    })
+})
 
+//GET - Returns an array of all the comment objects associated with the post with the specified id. --STATUS 404 not working
+router.get('/api/posts/:id/comments',  (req,res) => {
+    posts.findPostComments(req.params.id)
+        .then((comments)=>{
+            if(comments){
+                res.status(200).json(comments)
+            } else {
+                // //status 404 not working - returns  an empty array
+                res.status(404).json({
+                    message:'Post not found'
+                })
+            }
+        })
+        .catch((err)=>{
+            console.log(error)
+			res.status(500).json({
+				message: "Error retrieving the comments",
+			})
+        })
+})
 
-//GET - Returns an array of all the comment objects associated with the post with the specified id.
-
-
-//DELETE - Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
-
+//DELETE - Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement. --DONE
+router.delete('/api/posts/:id', (req,res) => {
+    posts.remove(req.params.id)
+        .then((post)=>{
+            if(post > 0){
+                res.status(200).json({
+                    message: "The post is destroyed successfully"
+                })
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch((error)=>{
+            console.log(error)
+                res.status(500).json({
+                    error: "The post could not be removed",
+                })
+        })
+})
 
 //PUT - Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+router.put('/api/posts/:id',  (req,res) => {
 
+})
 
 
 
